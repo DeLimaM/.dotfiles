1dde4923106b99ce741a351e7affae96db54dc63
Martin De Lima <martin.delimaa@gmail.com>
2026-02-14 20:48:51 +0100

    Auto-sync all tracked files

--- home/martin/.config/polybar/launch.sh
+++ home/martin/.config/polybar/launch.sh
@@ -1,7 +1,17 @@
-polybar-msg cmd quit
+#!/usr/bin/env bash
 
-echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar 2>&1 | tee -a /tmp/polybar1.log &
-disown
+# Kill old bars
+polybar-msg cmd quit
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 0.1; done
+
+# Launch one bar per connected monitor
+for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload main 2>&1 | tee -a /tmp/polybar-$m.log &
+done
 
 echo "Bars launched..."
+
+
